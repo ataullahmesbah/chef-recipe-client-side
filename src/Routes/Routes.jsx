d7990aad7d043dbe701c442d2e42309c6e9586ca
Ataullah Mesbah Fahim <mesbahataullah4@gmail.comm>
2023-05-02 23:39:08 +0600

    routes setup problem solve

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,12 +4,18 @@ import Navbar from "../Shared/Navbar/Navbar";
 import Blog from "../Pages/Blog/Blog";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
+import Main from "../Shared/Main/Main";
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Home></Home>,
         children: [
+
+            {
+                path: '/',
+                element: <Main></Main>
+            },
             
             {
                 path: '/blog',
---
+++ src/Shared/Main/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            
+        </div>
+    );
+};
+
+export default Main;
