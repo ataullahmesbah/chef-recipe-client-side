e2b722d065e4a3b4d30c75b30ddb0a792bfc1220
Ataullah Mesbah Fahim <mesbahataullah4@gmail.comm>
2023-05-04 23:10:54 +0600

    firebase hidden ena.local add

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+console.log('environment variable', import.meta.env.VITE_messagingSenderId);
 const firebaseConfig = {
-  apiKey: "AIzaSyCsALhbVDa1T63G1ymbD-bIspZ6z3r2HyQ",
-  authDomain: "assignment-project-cf4fe.firebaseapp.com",
-  projectId: "assignment-project-cf4fe",
-  storageBucket: "assignment-project-cf4fe.appspot.com",
-  messagingSenderId: "259390734364",
-  appId: "1:259390734364:web:0c93d12e9d55734fdf1fd9"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
